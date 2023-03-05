ef9e3eec07ba04482bbc23510aa0dd673868987c
Tin <tin.jurcevic@gmail.com>
2023-03-05 23:45:13 +0100

    Change SideNavbar to Navbar with Text

--- src/components/Layout/SideNavbar.tsx
+++ src/components/Layout/SideNavbar.tsx
@@ -2,67 +2,77 @@ import { useState } from "react";
 import {
   createStyles,
   Navbar,
-  UnstyledButton,
-  Tooltip,
-  Title,
+  Group,
+  Code,
+  getStylesRef,
   rem,
 } from "@mantine/core";
 import {
-  IconHome2,
-  IconGauge,
-  IconDeviceDesktopAnalytics,
+  IconBellRinging,
   IconFingerprint,
-  IconCalendarStats,
-  IconUser,
+  IconKey,
   IconSettings,
+  Icon2fa,
+  IconDatabaseImport,
+  IconReceipt2,
+  IconSwitchHorizontal,
+  IconLogout,
 } from "@tabler/icons-react";
 
 const useStyles = createStyles((theme) => ({
-  wrapper: {
-    display: "flex",
-  },
-
-  aside: {
-    flex: `0 0 ${rem(60)}`,
-    backgroundColor:
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    borderRight: `${rem(1)} solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[3]
+  header: {
+    paddingBottom: theme.spacing.md,
+    marginBottom: `calc(${theme.spacing.md} * 1.5)`,
+    borderBottom: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
     }`,
   },
 
-  main: {
-    flex: 1,
-    backgroundColor:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[6]
-        : theme.colors.gray[0],
+  footer: {
+    paddingTop: theme.spacing.md,
+    marginTop: theme.spacing.md,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
   },
 
-  mainLink: {
-    width: rem(44),
-    height: rem(44),
-    borderRadius: theme.radius.md,
+  link: {
+    ...theme.fn.focusStyles(),
     display: "flex",
     alignItems: "center",
-    justifyContent: "center",
+    textDecoration: "none",
+    fontSize: theme.fontSizes.sm,
     color:
       theme.colorScheme === "dark"
-        ? theme.colors.dark[0]
+        ? theme.colors.dark[1]
         : theme.colors.gray[7],
+    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
+    borderRadius: theme.radius.sm,
+    fontWeight: 500,
 
     "&:hover": {
       backgroundColor:
         theme.colorScheme === "dark"
-          ? theme.colors.dark[5]
+          ? theme.colors.dark[6]
           : theme.colors.gray[0],
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
+
+      [`& .${getStylesRef("icon")}`]: {
+        color: theme.colorScheme === "dark" ? theme.white : theme.black,
+      },
     },
   },
 
-  mainLinkActive: {
+  linkIcon: {
+    ref: getStylesRef("icon"),
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[2]
+        : theme.colors.gray[6],
+    marginRight: theme.spacing.sm,
+  },
+
+  linkActive: {
     "&, &:hover": {
       backgroundColor: theme.fn.variant({
         variant: "light",
@@ -70,152 +80,72 @@ const useStyles = createStyles((theme) => ({
       }).background,
       color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
         .color,
-    },
-  },
-
-  title: {
-    boxSizing: "border-box",
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    marginBottom: theme.spacing.xl,
-    backgroundColor:
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-    padding: theme.spacing.md,
-    paddingTop: rem(18),
-    height: rem(60),
-    borderBottom: `${rem(1)} solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[3]
-    }`,
-  },
-
-  logo: {
-    boxSizing: "border-box",
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    height: rem(60),
-    paddingTop: theme.spacing.md,
-    borderBottom: `${rem(1)} solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[3]
-    }`,
-    marginBottom: theme.spacing.xl,
-  },
-
-  link: {
-    boxSizing: "border-box",
-    display: "block",
-    textDecoration: "none",
-    borderTopRightRadius: theme.radius.md,
-    borderBottomRightRadius: theme.radius.md,
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    padding: `0 ${theme.spacing.md}`,
-    fontSize: theme.fontSizes.sm,
-    marginRight: theme.spacing.md,
-    fontWeight: 500,
-    height: rem(44),
-    lineHeight: rem(44),
-
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[5]
-          : theme.colors.gray[1],
-      color: theme.colorScheme === "dark" ? theme.white : theme.black,
-    },
-  },
-
-  linkActive: {
-    "&, &:hover": {
-      borderLeftColor: theme.fn.variant({
-        variant: "filled",
-        color: theme.primaryColor,
-      }).background,
-      backgroundColor: theme.fn.variant({
-        variant: "filled",
-        color: theme.primaryColor,
-      }).background,
-      color: theme.white,
+      [`& .${getStylesRef("icon")}`]: {
+        color: theme.fn.variant({ variant: "light", color: theme.primaryColor })
+          .color,
+      },
     },
   },
 }));
 
-const mainLinksMockdata = [
-  { icon: IconHome2, label: "Home" },
-  { icon: IconGauge, label: "Dashboard" },
-  { icon: IconDeviceDesktopAnalytics, label: "Analytics" },
-  { icon: IconCalendarStats, label: "Releases" },
-  { icon: IconUser, label: "Account" },
-  { icon: IconFingerprint, label: "Security" },
-  { icon: IconSettings, label: "Settings" },
-];
-
-const linksMockdata = [
-  "Security",
-  "Settings",
-  "Dashboard",
-  "Releases",
-  "Account",
-  "Orders",
-  "Clients",
-  "Databases",
-  "Pull Requests",
-  "Open Issues",
-  "Wiki pages",
+const data = [
+  { link: "", label: "Notifications", icon: IconBellRinging },
+  { link: "", label: "Billing", icon: IconReceipt2 },
+  { link: "", label: "Security", icon: IconFingerprint },
+  { link: "", label: "SSH Keys", icon: IconKey },
+  { link: "", label: "Databases", icon: IconDatabaseImport },
+  { link: "", label: "Authentication", icon: Icon2fa },
+  { link: "", label: "Other Settings", icon: IconSettings },
 ];
 
 const SideNavbar = () => {
   const { classes, cx } = useStyles();
-  const [active, setActive] = useState("Releases");
-  const [activeLink, setActiveLink] = useState("Settings");
+  const [active, setActive] = useState("Billing");
 
-  const mainLinks = mainLinksMockdata.map((link) => (
-    <Tooltip
-      label={link.label}
-      position='right'
-      withArrow
-      transitionProps={{ duration: 0 }}
-      key={link.label}
-    >
-      <UnstyledButton
-        onClick={() => setActive(link.label)}
-        className={cx(classes.mainLink, {
-          [classes.mainLinkActive]: link.label === active,
-        })}
-      >
-        <link.icon size='1.4rem' stroke={1.5} />
-      </UnstyledButton>
-    </Tooltip>
-  ));
-
-  const links = linksMockdata.map((link) => (
+  const links = data.map((item) => (
     <a
       className={cx(classes.link, {
-        [classes.linkActive]: activeLink === link,
+        [classes.linkActive]: item.label === active,
       })}
-      href='/'
+      href={item.link}
+      key={item.label}
       onClick={(event) => {
         event.preventDefault();
-        setActiveLink(link);
+        setActive(item.label);
       }}
-      key={link}
     >
-      {link}
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
     </a>
   ));
 
   return (
-    <Navbar height={750} width={{ sm: 300 }}>
-      <Navbar.Section grow className={classes.wrapper}>
-        <div className={classes.aside}>{mainLinks}</div>
-        <div className={classes.main}>
-          <Title order={4} className={classes.title}>
-            {active}
-          </Title>
+    <Navbar height={700} width={{ sm: 300 }} p='md'>
+      <Navbar.Section grow>
+        <Group className={classes.header} position='apart'>
+          <Code sx={{ fontWeight: 700 }}>v3.1.2</Code>
+        </Group>
+        {links}
+      </Navbar.Section>
 
-          {links}
-        </div>
+      <Navbar.Section className={classes.footer}>
+        <a
+          href='#'
+          className={classes.link}
+          onClick={(event) => event.preventDefault()}
+        >
+          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
+          <span>Change account</span>
+        </a>
+
+        <a
+          href='#'
+          className={classes.link}
+          onClick={(event) => event.preventDefault()}
+        >
+          <IconLogout className={classes.linkIcon} stroke={1.5} />
+          <span>Logout</span>
+        </a>
       </Navbar.Section>
     </Navbar>
   );
