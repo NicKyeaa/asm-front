0d5d7d5c3a72e8c5ba0dd87d403bb34d784b75e2
NicKyeaa <tin.jurcevic@gmail.com>
2023-03-12 16:29:43 +0100

    Added Ant design component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import MainLayout from './components/Layout/MainLayout';
-import AssetsPage from './pages/AssetsPage/AssetsPage';
-import { Route, Routes } from 'react-router-dom';
+import MainLayout from "./pages/Homepage/MainLayout";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<MainLayout />}></Route>
-        <Route path="/assets" element={<AssetsPage />}></Route>
-      </Routes>
+    <div className='App'>
+      <MainLayout></MainLayout>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
