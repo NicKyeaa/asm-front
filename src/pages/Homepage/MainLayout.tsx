b29fdbbd91c456ce8132a7c4f329b09514da88fb
NicKyeaa <tin.jurcevic@gmail.com>
2023-04-23 18:11:28 +0200

    Added menu Items and icons

--- src/pages/Homepage/MainLayout.tsx
+++ src/pages/Homepage/MainLayout.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  LaptopOutlined,
-  NotificationOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { BookOutlined, ClusterOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Breadcrumb, Layout, Menu, theme } from "antd";
 // import { QueryClient, QueryClientProvider } from "react-query";
@@ -15,32 +11,18 @@ import AssetTable from "../AssetsPage/AssetTable";
 
 const { Header, Content, Sider } = Layout;
 
-const items1: MenuProps["items"] = ["1", "2", "3"].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-
 const items2: MenuProps["items"] = [
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
-].map((icon, index) => {
-  const key = String(index + 1);
-
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
-});
+  {
+    key: "1",
+    icon: <BookOutlined />,
+    label: "Work orders",
+  },
+  {
+    key: "2",
+    icon: <ClusterOutlined />,
+    label: "Assets",
+  },
+];
 
 const MainLayout: React.FC = () => {
   const {
@@ -61,7 +43,7 @@ const MainLayout: React.FC = () => {
           theme='dark'
           mode='horizontal'
           defaultSelectedKeys={["2"]}
-          items={items1}
+          items={items2}
         />
       </Header>
       <Layout>
