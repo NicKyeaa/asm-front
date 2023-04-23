8d40f3ec04a3aaab1cf92914a0ddb00d228d12d6
NicKyeaa <tin.jurcevic@gmail.com>
2023-04-23 18:28:35 +0200

    Changing layout

--- src/pages/Homepage/MainLayout.tsx
+++ src/pages/Homepage/MainLayout.tsx
@@ -28,48 +28,37 @@ const MainLayout: React.FC = () => {
 
   return (
     <Layout>
-      <Header className='header'>
-        <div className='logo' />
+      <Sider
+        width={200}
+        style={{
+          background: colorBgContainer,
+          position: "fixed",
+        }}
+      >
         <Menu
-          theme='dark'
-          mode='horizontal'
-          defaultSelectedKeys={["2"]}
+          mode='inline'
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          style={{ height: "100%", borderRight: 0 }}
           items={items2}
         />
-      </Header>
-      <Layout>
-        <Sider
-          width={200}
+      </Sider>
+      <Layout style={{ padding: "0 24px 24px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
           style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
             background: colorBgContainer,
-            position: "fixed",
           }}
         >
-          <Menu
-            mode='inline'
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "100%", borderRight: 0 }}
-            items={items2}
-          />
-        </Sider>
-        <Layout style={{ padding: "0 24px 24px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-              background: colorBgContainer,
-            }}
-          >
-            <AssetTable />
-          </Content>
-        </Layout>
+          <AssetTable />
+        </Content>
       </Layout>
     </Layout>
   );
