6df674bbe86dcc82de6371a4a906499a000cd3b3
NicKyeaa <tin.jurcevic@gmail.com>
2023-05-02 22:21:09 +0200

    Added Tab Menu to the Card of Assigned Component

--- src/pages/WorkOrdersPage/WorkOrders.tsx
+++ src/pages/WorkOrdersPage/WorkOrders.tsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
-import { Col, Row } from "antd";
+import { Col, Row, Card } from "antd";
+
+const tabList = [
+  {
+    key: "To Do",
+    tab: "To Do",
+  },
+  {
+    key: "Done",
+    tab: "Done",
+  },
+];
 
 const WorkOrders: React.FC = () => {
   return (
     <Row gutter={6}>
       <Col span={6} offset={2}>
-        Col
+        <Card title='Work Orders' tabList={tabList}></Card>
       </Col>
       <Col span={6} offset={2}>
         Col
